2045b4b18aa1d23b39ad5fbec96d9122a15d601b
Takeuchi-art <153795269+Takeuchi-art@users.noreply.github.com>
2024-01-18 13:44:05 +0900

    キャラクター情報の作成
    キャラクターのスキルを作りました

---
+++ character damage result/character.py
@@ -0,0 +1,88 @@
+{
+  "characters": [
+    {
+      "name": "勇者",
+      "class": "戦士",
+      "skills": [
+        {
+          "name": "剣の一閃",
+          "mp_cost": 10,
+          "damage": 20
+        },
+        {
+          "name": "守りの姿勢",
+          "mp_cost": 5,
+          "defense_boost": 10
+        },
+        {
+          "name": "連続斬り",
+          "mp_cost": 15,
+          "damage": 35
+        }
+      ]
+    },
+    {
+      "name": "魔法使い",
+      "class": "魔法使い",
+      "skills": [
+        {
+          "name": "火の玉",
+          "mp_cost": 12,
+          "damage": 25
+        },
+        {
+          "name": "氷の結界",
+          "mp_cost": 8,
+          "defense_boost": 15
+        },
+        {
+          "name": "雷の魔法",
+          "mp_cost": 18,
+          "damage": 40
+        }
+      ]
+    },
+    {
+      "name": "剣士",
+      "class": "戦士",
+      "skills": [
+        {
+          "name": "突進斬り",
+          "mp_cost": 15,
+          "damage": 30
+        },
+        {
+          "name": "かばう",
+          "mp_cost": 10,
+          "defense_boost": 20
+        },
+        {
+          "name": "回し斬り",
+          "mp_cost": 20,
+          "damage": 45
+        }
+      ]
+    },
+    {
+      "name": "僧侶",
+      "class": "癒し手",
+      "skills": [
+        {
+          "name": "癒しの手",
+          "mp_cost": 8,
+          "healing": 30
+        },
+        {
+          "name": "祈りの光",
+          "mp_cost": 12,
+          "healing": 50
+        },
+        {
+          "name": "清めの泉",
+          "mp_cost": 15,
+          "healing": 70
+        }
+      ]
+    }
+  ]
+}
