e4e40826599c086f37e795f0e215e0eb5c8efd6c
yuto-dukshund <yu.20030909.to@icloud.com>
2024-01-25 15:48:29 +0900

    ushida_char
    character.py変更

--- character damage result/Character.py
+++ character damage result/Character.py
@@ -3,10 +3,11 @@ import random
 #名前、ヒットポイント（hp）、攻撃力などの属性を持っている
 
 class Character:
-    def __init__(self, name, hp, attack):
+    def __init__(self, name, hp, attack, mp):
         self.name = name
         self.hp = hp
         self.attack = attack
+        self.mp = mp
 
     def is_alive(self):
         return self.hp > 0
@@ -19,5 +20,10 @@ class Character:
 
     def attack_other(self, other):
         damage = random.randint(0, self.attack)
-        print(f"{self.name} の通常攻撃 {other.name} に {damage} のダメージ!")
+        print(f"\n{self.name} の通常攻撃 : {other.name} に {damage} のダメージ!")
         other.get_hit(damage)
+    #プレイヤー
+    def getHP(self):
+        return self.hp
+    def getMP(self):
+        return self.mp
