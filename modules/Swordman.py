4da58f6de3fc32cf2dc41f286694db20c8d09fa4
K22012 <k22012kk@aitech.ac.jp>
2024-01-27 16:34:47 +0900

    ito_swordsman

--- modules/Swordman.py
+++ modules/Swordman.py
@@ -2,15 +2,15 @@ from Character import Character
 import random
 
 class Swordsman(Character):
-    def __init__(self, name, hp, attack):
-        super().__init__(name, hp, attack)
+    def __init__(self, name, hp, attack, mp):
+        super().__init__(name, hp, attack, mp)
 
     def special_attack(self, other):
         special_damage = self.attack * 2
-        print(f"{self.name} の強力な突進! {other.name} に {special_damage} のダメージ!")
+        print(f"\n{self.name} の強力な突進! {other.name} に {special_damage} のダメージ!")
         other.get_hit(special_damage)
 
 
     def block(self):
         block_value = random.randint(5, 10)
-        print(f"{self.name} が防御態勢をとった! 次の攻撃からのダメージを軽減します。 (軽減量: {block_value})")
+        print(f"\n{self.name} が防御態勢をとった! 次の攻撃からのダメージを軽減! (軽減量: {block_value})")
